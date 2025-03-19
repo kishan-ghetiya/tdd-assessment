bd08d6be8a0894fac7bef8984e4a74dd3c27ae54
Kishan Patel <kishan@sixberries.com>
2025-03-19 22:16:47 +0530

    Single number returns that number

--- calculator.js
+++ calculator.js
@@ -1,5 +1,6 @@
 function add(numbers) {
   if (!numbers) return 0;
+  if (!numbers.includes(",")) return parseInt(numbers);
   return 0;
 }
 
--- calculator.test.js
+++ calculator.test.js
@@ -1,5 +1,9 @@
-const { add } = require('./calculator');
+const { add } = require("./calculator");
 
-test('should return 0 for empty string', () => {
-  expect(add('')).toBe(0);
+test("should return 0 for empty string", () => {
+  expect(add("")).toBe(0);
+});
+
+test("should return number itself for a single number", () => {
+  expect(add("1")).toBe(1);
 });
