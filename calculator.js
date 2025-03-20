da05fc212d400b9cacd42c0b9636500cd39a6f56
Kishan Patel <kishan@sixberries.com>
2025-03-20 11:06:08 +0530

    fixed message

--- calculator.js
+++ calculator.js
@@ -13,7 +13,7 @@ function add(numbers) {
 
   const negatives = numArray.filter((num) => num < 0);
   if (negatives.length) {
-    throw new Error(`Negative numbers not allowed ${negatives.join(",")}`);
+    throw new Error(`negative numbers not allowed ${negatives.join(",")}`);
   }
 
   return numArray.reduce((sum, num) => sum + num, 0);
